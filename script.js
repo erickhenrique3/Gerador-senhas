5c665342f8b1d104a2f429992f00667ce586419d
erickhenrique3 <erick.2henriquehtp@gmail.com>
2023-07-21 14:47:58 -0300

    ff

---
+++ script.js
@@ -0,0 +1,36 @@
+let sliderElement = document.querySelector("#slider");
+let buttonelement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#passoword");
+
+
+let containerPassword = document.querySelector("#container-passowrd")
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+let novaSenha = "";
+
+sizePassword.innerHTML =sliderElement.value;
+
+slider.oninput = function() {
+    sizePassword.innerHTML = this.value;
+  }
+  
+  
+  function generatePassword(){
+  
+    let pass = '';
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+      pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+    
+    console.log(pass)
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha = pass;
+  }
+  
+  function copyPassword(){
+    alert("Senha copiada com sucesso!")
+    navigator.clipboard.writeText(novaSenha);
+  }
